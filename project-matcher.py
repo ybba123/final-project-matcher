61e4fbaf693f86e64a48f38324a3120e2d5d9881
cw111-gh <cw111@wellesley.edu>
2025-04-28 11:47:54 -0400

    added project matcher

--- project-matcher.py
+++ project-matcher.py
@@ -4,3 +4,11 @@ from z3 import *
 
 s = Solver()
 
+# pseudocode
+# all our constraints: topic areas of interest, preferences for being partners with
+# certain people, availability, and desired grade range for the final project
+# unit propagation and backtracking search
+
+# don't match together people who hate each other
+def no_hatred(formula):
+    # if person A hates person B, it's mutually exclusive and A ^ B would be false
