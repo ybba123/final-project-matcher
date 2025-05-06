d819a8a06642742faf7dfd1b34d656cd2a2f101f
agnosthesia739 <119769680+agnosthesia739@users.noreply.github.com>
2025-05-06 14:41:00 -0400

    Update project-matcher.py

--- project-matcher.py
+++ project-matcher.py
@@ -8,7 +8,54 @@ s = Solver()
 # all our constraints: topic areas of interest, preferences for being partners with
 # certain people, availability, and desired grade range for the final project
 # unit propagation and backtracking search
+# send to z3: there is a number of groups, each person is a variable and gets assigned a group number
+# data structures: 
+#   input in python
+#   list of dictionaries for each person with their preferences, {int (each person's ID): {"hate": [1, 2, 3];
+#   "times": ["Monday morning"]; "topics": ["garbage collection"]}}
+# convert each person's preferences into a formula with clauses
+#
+# z3 is a SAT solver so don't need to implement a whole SAT solver
 
-# don't match together people who hate each other
-def no_hatred(formula):
+# don't match together people who hate (A's group number and B's group number must be different) each other
+def no_hatred(a, b):
     # if person A hates person B, it's mutually exclusive and A ^ B would be false
+    # hatred can be one directional
+    s = Solver()
+    x = Bool('hate')
+
+    a = Int('person a')
+    b = Int('person b')
+
+    s.add(Not(And(a, b)))
+
+    result = s.check() # check result
+
+    if result == sat:
+        print("sat")
+    else:
+        print("unsat. person a and person b cannot be paired together")
+        print(s.model())
+
+def common_preferences(a_pref, b_pref):
+    # if person A and person B have no topics in common, then they cannot be paired together
+    s = Solver()
+    common_pref = False
+    # check if A and B have topics in common
+    for topic in a_pref:
+        for topic2 in b_pref:
+            if topic == topic2:
+                common_topic = True
+                break
+
+    s.add(common_topic)
+
+    result = s.check() # check result
+
+    if result == sat:
+        print("sat")
+    else:
+        print("unsat. person a and person b cannot be paired together")
+        print(s.model())
+
+def common_time(a)
