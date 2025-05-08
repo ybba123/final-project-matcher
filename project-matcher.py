805c080ed2e67a464c884b58df076ac85849a443
Abby Brennan-Jones <ab129@wellesley.edu>
2025-05-08 13:22:54 -0400

    implemented Alexa's suggestions (Abby Brennan-Jones)

--- project-matcher.py
+++ project-matcher.py
@@ -87,32 +87,79 @@ def common_time(a_pref, b_pref, c_pref):
     #     print(s.model())
 
 def main():
-    a_json = input("Enter preferences json for student a: ") # TODO: make this prompt more user-friendly
-    b_json = input("Enter preferences json for student b: ")
-    c_json = input("Enter preferences json for student c: ")
-    a_dict = json.loads(a_json)
-    b_dict = json.loads(b_json)
-    c_dict = json.loads(c_json)
-    preferences = [a_dict,b_dict,c_dict]
+    # TODO: make this prompt more user-friendly
+    # TODO: make it so the user can input more than 3 student JSONs; make it so input isn't hard-coded
+
+    # a_json = input("Enter preferences json for student a: ")
+
+    # b_json = input("Enter preferences json for student b: ")
+    # c_json = input("Enter preferences json for student c: ")
+    # a_dict = json.loads(a_json)
+    # b_dict = json.loads(b_json)
+    # c_dict = json.loads(c_json)
+    # preferences = [a_dict,b_dict,c_dict]
     # preferences = [{"name":"a", "hate":["b,c"], "topics":["garbage collection", "assembly/ISA"], "times":["Monday morning"]},
     #                {"name":"b", "hate":[], "topics":["garbage collection"], "times":["Tuesday evening"]},
-    #                {"name":"c", "hate":[], "topics":["networks"], "times":["Wednesday morning"]}]
-                #     {'name':'d', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
-                #    {'name':'e', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
-                #    {'name':'f', 'hate':[], 'topics':['networks'], 'times':['Friday morning']}
-                
-    # for student in preferences:
-    s = Solver()
+    #                {"name":"c", "hate":[], "topics":["networks"], "times":["Wednesday morning"]},
+    #                 {'name':'d', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+    #                {'name':'e', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+    #                {'name':'f', 'hate':[], 'topics':['networks'], 'times':['Friday morning']}]
+    
+    preferences = [{"name":"a", "hate":[], "topics":["garbage collection", "assembly/ISA"], "times":["Monday morning"]},
+                   {"name":"b", "hate":[], "topics":["garbage collection"], "times":["Friday morning"]},
+                   {"name":"c", "hate":[], "topics":["garbage collection"], "times":["Friday morning"]},
+                    {'name':'d', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+                   {'name':'e', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+                   {'name':'f', 'hate':[], 'topics':['garbage collection'], 'times':['Friday morning']}]
+    
+    # preferences = [{"name":"a", "hate":[], "topics":["garbage collection", "assembly/ISA"], "times":["Monday morning"]},
+    #                {"name":"b", "hate":[], "topics":["garbage collection"], "times":["Monday morning"]},
+    #                {"name":"c", "hate":[], "topics":["garbage collection"], "times":["Monday morning"]}]
 
-    s.add(no_hatred(preferences[0],preferences[1],preferences[2]))
-    s.add(common_topic(preferences[0],preferences[1],preferences[2]))
-    s.add(common_time(preferences[0],preferences[1],preferences[2]))
+    s = Solver()
+    num_students = len(preferences)
+    print("num_students",num_students)
+    num_groups = math.floor(num_students/3)
+    print("num_groups",num_groups)
+
+    # assign ints to students
+    for i in range(len(preferences)):
+        group_num = Int(f'group_num_{i}') # program breaks if you change this to 'group_num'; the index is important apparently
+        # add constraint for size
+        s.add(group_num >= 0)
+        s.add(group_num < num_groups)
+        preferences[i]["group number"] = group_num
+        # print(preferences[i]["group number"])
+
+    for student in preferences:
+        for student2 in preferences:
+            if student2 == student:
+                continue
+            for student3 in preferences:
+                if student3 == student or student3 == student2:
+                    continue
+                # check if they have the same group number. If they do, this implies that constraints are true
+                s.add(Implies(student["group number"] == student2["group number"],
+                            And(common_topic(student,student2,student3),
+                                And(no_hatred(student,student2,student3),
+                                    common_time(student,student2,student3)))))
 
     result = s.check()
 
     if result == sat:
-        print(f"Group 1: Student {preferences[0]['name']}, Student {preferences[1]['name']}, and Student {preferences[2]['name']}")
+        model = s.model()
+        # print(f"Group 1: Student {preferences[0]['name']}, Student {preferences[1]['name']}, and Student {preferences[2]['name']}")
+        for i in range(num_groups):
+            print(f"\nGroup {i}:")
+            for student in preferences:
+                # print(student["group number"])
+                # print(model.evaluate(student["group number"]))
+                if model.evaluate(student["group number"]) == i:
+                    print(student["name"])
     else:
         print("Students are incompatible.")
 
 main()
+
+# TODO: add testing
+# TODO: clean up file, document
