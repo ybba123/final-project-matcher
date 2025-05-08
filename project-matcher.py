e9fb29904598d7248fd64c35735eae94adfb5692
Abby Brennan-Jones <ab129@wellesley.edu>
2025-05-07 22:41:48 -0400

    fixed bugs in constraint functions, converted input format to JSON (Abby Brennan-Jones)

--- project-matcher.py
+++ project-matcher.py
@@ -1,6 +1,7 @@
 # Authors: Abby Brennan-Jones, Catherine Wang
 
 from z3 import *
+import json
 
 s = Solver()
 
@@ -18,7 +19,7 @@ s = Solver()
 # z3 is a SAT solver so don't need to implement a whole SAT solver
 
 # don't match together people who hate (A's group number and B's group number must be different) each other
-def no_hatred(a, b, c):
+def no_hatred(a_pref, b_pref, c_pref):
     # if person A hates person B, it's mutually exclusive and A ^ B would be false
     # hatred can be one directional
     # s = Solver()
@@ -36,24 +37,35 @@ def no_hatred(a, b, c):
     # else:
     #     print("unsat. person a and person b cannot be paired together")
     #     print(s.model())
-    for student in a['hate']:
+    for student in a_pref['hate']:
         if student == 'b' or student == 'c':
             return False
-    for student in b['hate']:
+    for student in b_pref['hate']:
         if student == 'a' or student == 'c':
             return False
-    for student in c['hate']:
+    for student in c_pref['hate']:
         if student == 'a' or student == 'b':
             return False
     return True
 
-def common_preferences(a_pref, b_pref, c_pref):
+def common_topic(a_pref, b_pref, c_pref):
     # if person A and person B have no topics in common, then they cannot be paired together
     s = Solver()
     common_pref = False
     # check if A and B have topics in common
-    for item in a_pref:
-        if item in b_pref and c_pref:
+    for topic in a_pref['topics']:
+        if topic in b_pref['topics'] and c_pref['topics']:
+            return True
+        
+    return False
+
+def common_time(a_pref, b_pref, c_pref):
+    # if person A and person B have no topics in common, then they cannot be paired together
+    s = Solver()
+    common_pref = False
+    # check if A and B have topics in common
+    for topic in a_pref['times']:
+        if topic in b_pref['times'] and c_pref['times']:
             return True
         
     return False
@@ -75,12 +87,32 @@ def common_preferences(a_pref, b_pref, c_pref):
     #     print(s.model())
 
 def main():
-    # preferences = input("Enter input: ") # TODO: make this prompt more user-friendly
-    preferences = [{'name':'a', 'hate':['b','c'], 'topics':['garbage collection', 'assembly/ISA'], 'times':['Monday morning', 'Monday evening']},
-                   {'name':'b', 'hate':[], 'topics':['garbage collection'], 'times':['Tuesday evening']},
-                   {'name':'c', 'hate':[], 'topics':['networks'], 'times':['Wednesday morning']},
-                   {'name':'d', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
-                   {'name':'e', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
-                   {'name':'f', 'hate':[], 'topics':['networks'], 'times':['Friday morning']}]
+    a_json = input("Enter preferences json for student a: ") # TODO: make this prompt more user-friendly
+    b_json = input("Enter preferences json for student b: ")
+    c_json = input("Enter preferences json for student c: ")
+    a_dict = json.loads(a_json)
+    b_dict = json.loads(b_json)
+    c_dict = json.loads(c_json)
+    preferences = [a_dict,b_dict,c_dict]
+    # preferences = [{"name":"a", "hate":["b,c"], "topics":["garbage collection", "assembly/ISA"], "times":["Monday morning"]},
+    #                {"name":"b", "hate":[], "topics":["garbage collection"], "times":["Tuesday evening"]},
+    #                {"name":"c", "hate":[], "topics":["networks"], "times":["Wednesday morning"]}]
+                #     {'name':'d', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+                #    {'name':'e', 'hate':[], 'topics':['networks','garbage collection'], 'times':['Monday morning']},
+                #    {'name':'f', 'hate':[], 'topics':['networks'], 'times':['Friday morning']}
     
     # for student in preferences:
+    s = Solver()
+
+    s.add(no_hatred(preferences[0],preferences[1],preferences[2]))
+    s.add(common_topic(preferences[0],preferences[1],preferences[2]))
+    s.add(common_time(preferences[0],preferences[1],preferences[2]))
+
+    result = s.check()
+
+    if result == sat:
+        print(f"Group 1: Student {preferences[0]['name']}, Student {preferences[1]['name']}, and Student {preferences[2]['name']}")
+    else:
+        print("Students are incompatible.")
+
+main()
